634bef742b9caa0b8e0e260eedceb1917b313739
Gary <gurugaryc@gmail.com>
2023-11-27 12:10:45 +0000

    Set up rudimentary search bar, set up rudimentary filter icon, progress on stylesheet

---
+++ src/main/java/com/Team4/SmartTowns/searchController.java
@@ -0,0 +1,18 @@
+package com.Team4.SmartTowns;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class searchController {
+
+    @GetMapping("/search")
+    public String search(@RequestParam(name = "query", required = false) String query, Model model) {
+        model.addAttribute("searchQuery", query);
+        // Add search logic here
+
+        return "search_trails"; // name of the Thymeleaf template
+    }
+}
