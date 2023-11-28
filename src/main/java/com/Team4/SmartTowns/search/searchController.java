754b04040c3bce2370b045a0a6e9dfaba5399bfc
WeeblyMon <gurugaryc@gmail.com>
2023-11-28 08:41:20 +0000

    Backend Progress: Connected search controller and service

--- src/main/java/com/Team4/SmartTowns/search/searchController.java
+++ src/main/java/com/Team4/SmartTowns/search/searchController.java
@@ -1,18 +1,31 @@
 package com.Team4.SmartTowns.search;
 
+import com.Team4.SmartTowns.trails.model.Trail;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class searchController {
 
+    private final searchService searchService;
+
+    @Autowired
+    public searchController(searchService searchService) {
+        this.searchService = searchService;
+    }
+
     @GetMapping("/search")
     public String search(@RequestParam(name = "query", required = false) String query, Model model) {
-        model.addAttribute("searchQuery", query);
-        // Add search logic here
-
-        return "search/search_trails"; // name of the Thymeleaf template
+        List<Trail> trails = searchService.searchTrails(query);
+        model.addAttribute("trails", trails);
+        return "search_trails"; // Name of your Thymeleaf template
     }
+
+    // Other methods...
 }
+
