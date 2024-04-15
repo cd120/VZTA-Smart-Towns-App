1cc3cccf3684cee218bad55a2062e4d6f2b9a6dd
James Pang <pangjy@cardiff.ac.uk>
2024-04-15 01:57:33 +0000

    Update 4 files
    - /Terraform/c23077813-openrc.sh
    - /Terraform/server1.sh
    - /Terraform/plan.tf
    - /Terraform/variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "flavor" { default = "m1.large" }
+variable "image" { default = "Debian Buster 10.11.1 20211029" }
+variable "name1" { default = "DebianSmartTowns" }
+
+variable "keypair" { default = "username_keypair" } # you may need to change this
+variable "network" { default = "c23077813_network" }   # you need to change this
+
+variable "pool" { default = "cscloud_private_floating" }
+variable "server1_script" { default = "./server1.sh" }
+variable "security_description" { default = "Terraform security group" }
+variable "security_name" { default = "tf_securityMat" }
