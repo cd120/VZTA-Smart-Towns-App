31cd0a8e5cc3ad80babe9c2cf4786c015e68b67f
James Pang <pangjy@cardiff.ac.uk>
2024-04-16 17:57:17 +0000

    testing mariadb

--- Terraform/serverJenkins.sh
+++ Terraform/serverJenkins.sh
@@ -7,8 +7,7 @@ cd /home/debian
 echo logged in as $USER.
 echo in directory $PWD
 
-cd /home/debian
-echo in directory $PWD
+sudo apt update && sudo apt upgrade -y
 
 echo "--------Installing wget--------"
 sudo apt install wget -y
@@ -45,72 +44,69 @@ sudo mysql -u root -e "GRANT ALL PRIVILEGES on *.* TO root@localhost IDENTIFIED 
 # echo "Moving to root account..."
 # cd root
 
-# sudo touch /var/lib/jenkins/.ssh/known_hosts
-# sudo ssh-keyscan git.cardiff.ac.uk >> /var/lib/jenkins/.ssh/known_hosts
-# sudo chmod 644 /var/lib/jenkins/.ssh/known_hosts
 
-cat << `EOF` >> gitlab_project_keypair2.key
------BEGIN OPENSSH PRIVATE KEY-----
-b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
-NhAAAAAwEAAQAAAYEA02aAQWa4aVxW4tXTmajS1JdRPIRYeqYxlAXu4HkTuTwetBHy7kwS
-2EajFy97erElfw/xBbIilnyH+ChENzg1twYYJ7AcpC8KevBgREWCsz1eHpzSTlg+2FUG2D
-6OFTpr2WSu+FWY2Fqm0vM8McorIADHQ72nDyDiN6TdY+3qgOUh5Ztm+msd5pLTDo7UQqSU
-BgelZcPlCCSF63P80gj/F1CPrXKGhvODfImC+a9b1iKas5FEX1CILRm3AubBkv62tMFGu0
-lVjwrYyEZsMf43FYBya3OmSneu6WwKLZf4kCTa0tHvNlEHL0PQisvnzGW5dp4nJvWytobh
-Wz76Q61scKJXctwfj6e7lKoAzFdiLJ3EbHMN0FT8iDrt9UEZOsqGQwI+E9rysO2dlSt7ja
-3+J9l2n7TBuWcwaADwSlyYwOz6qy19KdqwifEwAxOVJok1s2X66BH1eg/h64sJz3AR7AHM
-M0g+cRa54cxUrTmEwn069TwEfQUYETQTt47JoghtAAAFmALefJsC3nybAAAAB3NzaC1yc2
-EAAAGBANNmgEFmuGlcVuLV05mo0tSXUTyEWHqmMZQF7uB5E7k8HrQR8u5MEthGoxcve3qx
-JX8P8QWyIpZ8h/goRDc4NbcGGCewHKQvCnrwYERFgrM9Xh6c0k5YPthVBtg+jhU6a9lkrv
-hVmNhaptLzPDHKKyAAx0O9pw8g4jek3WPt6oDlIeWbZvprHeaS0w6O1EKklAYHpWXD5Qgk
-hetz/NII/xdQj61yhobzg3yJgvmvW9YimrORRF9QiC0ZtwLmwZL+trTBRrtJVY8K2MhGbD
-H+NxWAcmtzpkp3rulsCi2X+JAk2tLR7zZRBy9D0IrL58xluXaeJyb1sraG4Vs++kOtbHCi
-V3LcH4+nu5SqAMxXYiydxGxzDdBU/Ig67fVBGTrKhkMCPhPa8rDtnZUre42t/ifZdp+0wb
-lnMGgA8EpcmMDs+qstfSnasInxMAMTlSaJNbNl+ugR9XoP4euLCc9wEewBzDNIPnEWueHM
-VK05hMJ9OvU8BH0FGBE0E7eOyaIIbQAAAAMBAAEAAAGAJUXGSUG2NAVu4FG5HDeS7J69WQ
-EwpHdW+HVPV86hNqCt/SvI/FD3ZaCWssjRaaGALfX+lfWBeaz9QDjBRkcn2Vw+uAiqFByj
-KWfmYUMsNzQTuywFPID8m6HqMghdfsqtJ0hEPU7CQytAjKMMMu8lSWA5wezXXqh6mjr8Od
-lB5nX/0v7iWja2r+elRMTUEef4nVONyvKOGDBwWEI8CkBB0beHjZpKs9KNlVCdgi5z775s
-LkrnljKZv/HvvZg3+xXjiLb4BTpG35nka7O2NqZYIvPjoz6Nk/v1XVOd9vYXj8tXw/2i2l
-mEomIsMaMxAZF4Syr6SDpxFrhHCWPlTsLtngeIrxVEClVK0ONwojjBXqb830oN6DPMRg+B
-Wk7cumDZInZkW1eMoQwjr5zpbHLTSjveiLW9Q1UMXfybq0Jo/ZFWmTKyLFe3nuCHVC141S
-HlHGu2I9bf6GN+B9/6C51ho5eEPyA8EFvH6hEnbdPT+kRKCar2omxvI4Xtq9tTILHBAAAA
-wEl1ynxbmRK0nY1o9TM5SBsClTnUbZpcmtvoewThHE9KwcBCBMRjqj0wHMWaNbpWU0Cell
-AyCXvV4kc6t1y6nE5uPzRmcwTJsy0tTw1qcNLFJ4RlBfU1O1tARQbawhP1irNjeWpBc9TR
-U9ofsyVkhLvU1loMxaBAdTTn/+cI5YgpeZxVu/K80b8YAfLVwA4CSGMJX4Izx9zLXC3urp
-atP9a+MwwS3FsLyzkwYplDOJ0bJRscmlisFuIXxKWZzW8YWQAAAMEA9U08Evll/Q/MMP20
-kKZb84b9mseiq4/1xyzsQfiOFqCogYoWxVDNDSq3HE5HEjl4cpZkH5BoCNm1QI2vxepO5U
-rjWToTFU3yOBIC1+LGkGzltJg8O7214JjJpIrsjrNaPDjaBN84pdtrMpgDasJS0PDxR3Vq
-PRrcrfNO9hwg0vP4MpQuuDDkWNj41CopOtgbObqd215aEaXevNLf6S7Agk/2WQXHPBlo+b
-DgTr/rDmu4wTu2WckcpPqY5C6VkV39AAAAwQDcnsMdaqFYDP6CaUPGMlSOpMoQc6JCmI5S
-/N4DSGyU9+vhMKcaTO2AZMSJxuyXSl2U2djNd3lPmTz+YUvZrXvb07oNOjS4Wt5tUKRB1Y
-AVrjX68QN3tRuta7K1gv7wbPdyLpk2zDosjWJPxoNtbApnp2P9HkPazdnwwRe0qxKG7ERT
-ZOyKY/0CDnDQjvZ8eu3kgRoE0Gn/IAcj3QJRUgRnKmJBlFiTZSC+yW0UmtZLg3HPN+30YA
-mxA5hAjicrpzEAAAAcSUQrYzIzMDc3ODEzQERTQTEwRjYwQThGNTQ2MgECAwQFBgc=
------END OPENSSH PRIVATE KEY-----
-`EOF`
+# cat << `EOF` >> gitlab_project_keypair2.key
+# -----BEGIN OPENSSH PRIVATE KEY-----
+# b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
+# NhAAAAAwEAAQAAAYEA02aAQWa4aVxW4tXTmajS1JdRPIRYeqYxlAXu4HkTuTwetBHy7kwS
+# 2EajFy97erElfw/xBbIilnyH+ChENzg1twYYJ7AcpC8KevBgREWCsz1eHpzSTlg+2FUG2D
+# 6OFTpr2WSu+FWY2Fqm0vM8McorIADHQ72nDyDiN6TdY+3qgOUh5Ztm+msd5pLTDo7UQqSU
+# BgelZcPlCCSF63P80gj/F1CPrXKGhvODfImC+a9b1iKas5FEX1CILRm3AubBkv62tMFGu0
+# lVjwrYyEZsMf43FYBya3OmSneu6WwKLZf4kCTa0tHvNlEHL0PQisvnzGW5dp4nJvWytobh
+# Wz76Q61scKJXctwfj6e7lKoAzFdiLJ3EbHMN0FT8iDrt9UEZOsqGQwI+E9rysO2dlSt7ja
+# 3+J9l2n7TBuWcwaADwSlyYwOz6qy19KdqwifEwAxOVJok1s2X66BH1eg/h64sJz3AR7AHM
+# M0g+cRa54cxUrTmEwn069TwEfQUYETQTt47JoghtAAAFmALefJsC3nybAAAAB3NzaC1yc2
+# EAAAGBANNmgEFmuGlcVuLV05mo0tSXUTyEWHqmMZQF7uB5E7k8HrQR8u5MEthGoxcve3qx
+# JX8P8QWyIpZ8h/goRDc4NbcGGCewHKQvCnrwYERFgrM9Xh6c0k5YPthVBtg+jhU6a9lkrv
+# hVmNhaptLzPDHKKyAAx0O9pw8g4jek3WPt6oDlIeWbZvprHeaS0w6O1EKklAYHpWXD5Qgk
+# hetz/NII/xdQj61yhobzg3yJgvmvW9YimrORRF9QiC0ZtwLmwZL+trTBRrtJVY8K2MhGbD
+# H+NxWAcmtzpkp3rulsCi2X+JAk2tLR7zZRBy9D0IrL58xluXaeJyb1sraG4Vs++kOtbHCi
+# V3LcH4+nu5SqAMxXYiydxGxzDdBU/Ig67fVBGTrKhkMCPhPa8rDtnZUre42t/ifZdp+0wb
+# lnMGgA8EpcmMDs+qstfSnasInxMAMTlSaJNbNl+ugR9XoP4euLCc9wEewBzDNIPnEWueHM
+# VK05hMJ9OvU8BH0FGBE0E7eOyaIIbQAAAAMBAAEAAAGAJUXGSUG2NAVu4FG5HDeS7J69WQ
+# EwpHdW+HVPV86hNqCt/SvI/FD3ZaCWssjRaaGALfX+lfWBeaz9QDjBRkcn2Vw+uAiqFByj
+# KWfmYUMsNzQTuywFPID8m6HqMghdfsqtJ0hEPU7CQytAjKMMMu8lSWA5wezXXqh6mjr8Od
+# lB5nX/0v7iWja2r+elRMTUEef4nVONyvKOGDBwWEI8CkBB0beHjZpKs9KNlVCdgi5z775s
+# LkrnljKZv/HvvZg3+xXjiLb4BTpG35nka7O2NqZYIvPjoz6Nk/v1XVOd9vYXj8tXw/2i2l
+# mEomIsMaMxAZF4Syr6SDpxFrhHCWPlTsLtngeIrxVEClVK0ONwojjBXqb830oN6DPMRg+B
+# Wk7cumDZInZkW1eMoQwjr5zpbHLTSjveiLW9Q1UMXfybq0Jo/ZFWmTKyLFe3nuCHVC141S
+# HlHGu2I9bf6GN+B9/6C51ho5eEPyA8EFvH6hEnbdPT+kRKCar2omxvI4Xtq9tTILHBAAAA
+# wEl1ynxbmRK0nY1o9TM5SBsClTnUbZpcmtvoewThHE9KwcBCBMRjqj0wHMWaNbpWU0Cell
+# AyCXvV4kc6t1y6nE5uPzRmcwTJsy0tTw1qcNLFJ4RlBfU1O1tARQbawhP1irNjeWpBc9TR
+# U9ofsyVkhLvU1loMxaBAdTTn/+cI5YgpeZxVu/K80b8YAfLVwA4CSGMJX4Izx9zLXC3urp
+# atP9a+MwwS3FsLyzkwYplDOJ0bJRscmlisFuIXxKWZzW8YWQAAAMEA9U08Evll/Q/MMP20
+# kKZb84b9mseiq4/1xyzsQfiOFqCogYoWxVDNDSq3HE5HEjl4cpZkH5BoCNm1QI2vxepO5U
+# rjWToTFU3yOBIC1+LGkGzltJg8O7214JjJpIrsjrNaPDjaBN84pdtrMpgDasJS0PDxR3Vq
+# PRrcrfNO9hwg0vP4MpQuuDDkWNj41CopOtgbObqd215aEaXevNLf6S7Agk/2WQXHPBlo+b
+# DgTr/rDmu4wTu2WckcpPqY5C6VkV39AAAAwQDcnsMdaqFYDP6CaUPGMlSOpMoQc6JCmI5S
+# /N4DSGyU9+vhMKcaTO2AZMSJxuyXSl2U2djNd3lPmTz+YUvZrXvb07oNOjS4Wt5tUKRB1Y
+# AVrjX68QN3tRuta7K1gv7wbPdyLpk2zDosjWJPxoNtbApnp2P9HkPazdnwwRe0qxKG7ERT
+# ZOyKY/0CDnDQjvZ8eu3kgRoE0Gn/IAcj3QJRUgRnKmJBlFiTZSC+yW0UmtZLg3HPN+30YA
+# mxA5hAjicrpzEAAAAcSUQrYzIzMDc3ODEzQERTQTEwRjYwQThGNTQ2MgECAwQFBgc=
+# -----END OPENSSH PRIVATE KEY-----
+# `EOF`
 
-#Restricts access rights for the .key file.
-chmod 400 gitlab_project_keypair.key
+# #Restricts access rights for the .key file.
+# chmod 400 gitlab_project_keypair2.key
 
-#Obtaining Gitlab's public key and storing it in the known hosts file.
-echo "--------Communicating with Gitlab...--------"
-touch .ssh/known_hosts
-ssh-keyscan git.cardiff.ac.uk >> .ssh/known_hosts
-#Giving access to read and write of the file
-chmod 644 .ssh/known_hosts
+# #Obtaining Gitlab's public key and storing it in the known hosts file.
+# echo "--------Communicating with Gitlab...--------"
+# touch .ssh/known_hosts
+# ssh-keyscan git.cardiff.ac.uk >> .ssh/known_hosts
+# #Giving access to read and write of the file
+# chmod 644 .ssh/known_hosts
 
-echo "--------Installing Git--------"
-sudo apt install git -y
+# echo "--------Installing Git--------"
+# sudo apt install git -y
 
-echo "-----Cloning the repository from Gitlab-----"
-ssh-agent bash -c 'ssh-add gitlab_project_keypair.key && git clone git@git.cardiff.ac.uk:c23077813/team-4-smart-towns.git'
+# echo "-----Cloning the repository from Gitlab-----"
+# ssh-agent bash -c 'ssh-add gitlab_project_keypair.key && git clone git@git.cardiff.ac.uk:c23077813/team-4-smart-towns.git'
 
-echo "-------sql-------"
-sudo mysql -u root -pcomsc < /home/debian/team-4-smart-towns/src/main/resources/schema.sql
-sudo mysql -u root -pcomsc < /home/debian/team-4-smart-towns/src/main/resources/data.sql
+# echo "-------sql-------"
+# sudo mysql -u root -pcomsc < /home/debian/team-4-smart-towns/src/main/resources/schema.sql
+# sudo mysql -u root -pcomsc < /home/debian/team-4-smart-towns/src/main/resources/data.sql
 
-sudo apt-get update
+# sudo apt update -y
 
 # echo "-------Downloading Java 17--------"
 # wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb
