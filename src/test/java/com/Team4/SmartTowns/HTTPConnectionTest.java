6584e7bd6fd3cfe6aba11d27971fb2d22586917a
James Pang <Pangjy@cardiff.ac.uk>
2024-04-23 20:02:15 +0100

    stable testing branch

---
+++ src/test/java/com/Team4/SmartTowns/HTTPConnectionTest.java
@@ -0,0 +1,29 @@
+package com.Team4.SmartTowns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.web.client.RestTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HTTPConnectionTest {
+
+    @Value(value="${local.server.port}")
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testConnection() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/login", String.class)).contains ("Username");
+    }
+
+//    @Test
+//    public void testMockMVC() throws Exception {
+//        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/trails", String.class)).contains ("mockCardiff");
+//    }
+}
---
+++ src/test/java/com/Team4/SmartTowns/MockMVCTest.java
@@ -0,0 +1,41 @@
+package com.Team4.SmartTowns;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MockMVCTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    //Unit tests using a mock MVC
+    @Test
+    public void testLoginPageRendersCorrectly() throws Exception {
+        this.mockMvc.perform(get("/login"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Username:")));
+    }
+    //test using a mock MVC but a with a test database
+//    @Test
+//    public void testMockMVC() throws Exception {
+//        this.mockMvc.perform(get("/trails"))
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(containsString("mockCardiff")));
+//    }
+
+}
